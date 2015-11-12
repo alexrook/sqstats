48e0fe99602da129e8fe50c4fa2533884755c2ef
Alex Rook <rook.alex@gmail.com>
2015-11-12 14:23:54 +0200

    add reports repeat to list.html

--- www/src/main/webapp/js/controllers.js
+++ www/src/main/webapp/js/controllers.js
@@ -7,8 +7,22 @@ angular.module('sqstats.controllers', [])
         .controller('MainCtrl',
                 ['$scope', function ($scope) {
 
+
                     }])
         .controller('RepListCtrl',
-                ['$scope', function ($scope) {
+                ['$scope', '$http', function ($scope, $http) {
+
+                        $http({
+                            method: 'GET',
+                            url: window.appdeb.urlprefix + 'rs/raw/reports'
+                        }).then(function successCallback(response) {
+                            var x2js = new X2JS();
+                            $scope.reports = x2js.xml_str2json(response.data);
+                            $scope.reports=$scope.reports.map?$scope.reports.map:$scope.reports;
+                            console.log($scope.reports);   
+
+                        }, function errorCallback(response) {
+                            $scope.error = response;
+                        });
 
                     }]);
