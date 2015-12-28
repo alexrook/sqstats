183671d28c9021e0152d8d232f5bd1ba3f11b8fc
Alex Rook <rook.alex@gmail.com>
2015-12-28 11:02:44 +0200

    work with maintenance

--- www/src/main/webapp/js/maintenance.js
+++ www/src/main/webapp/js/maintenance.js
@@ -1,8 +1,28 @@
 'use strict';
 
+/*
+ * get raw xml from server and render it
+ */
+function getTabData(url) {
+    console.log(url);    
+}
+
 $(function() {
+    
+    //first this
+    $('#myTabs a[data-json-get]').click(function (e) {
+        var active=$(this).parent().hasClass('active');
+        console.log(active);
+        if (!active) {
+            getTabData($(this).attr('data-json-get'));
+        }
+        
+    });
+     
     $('#myTabs a').click(function (e) {
-             e.preventDefault()
-         $(this).tab('show')
-    })
+         e.preventDefault();
+         $(this).tab('show');
+    });
+    
+   
 });
