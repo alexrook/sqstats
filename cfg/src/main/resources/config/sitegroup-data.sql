646fac8ab86bc0427ac69d642235f8fc4b6a9919
Alex Rook <rook.alex@gmail.com>
2016-04-29 11:53:48 +0300

    add ipv4 regex to sitegroup, return 2-domain regex to prev version

--- cfg/src/main/resources/config/sitegroup-data.sql
+++ cfg/src/main/resources/config/sitegroup-data.sql
@@ -7,6 +7,9 @@ insert into sitegroup (id,regex,name) values(3,'.*(google|gstatic).*\.(com|ru|ua
 
 -- substr values
 insert into sitegroup (id,regex,substr,description)
+                values(99995,'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',true,
+                'считать отдельно ipv4-addresses');
+insert into sitegroup (id,regex,substr,description)
                 values(99996,'.*xn----itbjhccqit6f\.xn--p1ai',true,
                 'считать отдельно итоги для сайтов домена щелкино-рк.рф');
 insert into sitegroup (id,regex,substr,description)
@@ -16,5 +19,5 @@ insert into sitegroup (id,regex,substr,description)
                 values(99998,'.*livejournal\.com',true,
                 'считать отдельные итоги для ЖЖ-страниц');
 insert into sitegroup (id,regex,substr,description)
-                values(99999,'[^.0-9]*\.[^.0-9]+$',true,
+                values(99999,'[^.]*\.[^.]+$',true,
                 'return 2-level domain for default, i.e some.aaa.com ->aaa.com');
